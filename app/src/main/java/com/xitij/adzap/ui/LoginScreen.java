28aee76945185dcb9fe6cb6bd2d55a65deb5a59c
xitij <dd.droid@yahoo.com>
2015-04-19 16:32:48 +0530

    ngf

--- app/src/main/java/com/xitij/adzap/ui/LoginScreen.java
+++ app/src/main/java/com/xitij/adzap/ui/LoginScreen.java
@@ -27,20 +27,21 @@ import java.util.regex.Pattern;
 public class LoginScreen extends ActionBarActivity {
 
     private CircleDialog dialog;
-    private EditText etUname,etPassword;
+    private EditText etUname, etPassword;
     private TextView txtBtnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        etUname = (EditText)findViewById(R.id.etUname);
-       etPassword = (EditText)findViewById(R.id.etPassword);
-        txtBtnLogin = (TextView)findViewById(R.id.txtBtnLogin);
+        etUname = (EditText) findViewById(R.id.etUname);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        txtBtnLogin = (TextView) findViewById(R.id.txtBtnLogin);
 
-       etUname.setText("softeng.krishna@gmail.com");
+        etUname.setText("softeng.krishna@gmail.com");
 
-       txtBtnLogin.setOnClickListener(new View.OnClickListener() {
+        txtBtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 processValidateData();
@@ -50,79 +51,76 @@ public class LoginScreen extends ActionBarActivity {
 
     }
 
-private void processValidateData(){
-        if(isEdiTextEmpty(etUname)){
+    private void processValidateData() {
+        if (isEdiTextEmpty(etUname)) {
             etUname.setError("Please Enter User Email Id !!!");
-        }else if(isEdiTextEmpty(etPassword)){
+        } else if (isEdiTextEmpty(etPassword)) {
             etPassword.setError("Please Enter Password !!!");
-        }else if(!isEmailMatch(etUname)){
-            Toast.makeText(LoginScreen.this,"Please Enter Valid Email address !!!",Toast.LENGTH_LONG).show();
-        }
-        else{
+        } else if (!isEmailMatch(etUname)) {
+            Toast.makeText(LoginScreen.this, "Please Enter Valid Email address !!!", Toast.LENGTH_LONG).show();
+        } else {
             processLogin();
         }
 
     }
 
 
-private void processLogin(){
-    dialog= new CircleDialog(LoginScreen.this,0);
-    dialog.setCancelable(false);
-    dialog.show();
+    private void processLogin() {
+        dialog = new CircleDialog(LoginScreen.this, 0);
+        dialog.setCancelable(false);
+        dialog.show();
 
 
-    String email = etUname.getText().toString().trim();
-    String password = etPassword.getText().toString().trim();
+        String email = etUname.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
 
-    new CallWebService(AppConstants.USER_LOGIN+email+"/"+password,CallWebService.TYPE_JSONOBJECT){
+        new CallWebService(AppConstants.USER_LOGIN + email + "/" + password, CallWebService.TYPE_JSONOBJECT) {
 
-        @Override
-        public void response(String response) {
-            dialog.dismiss();
-            Log.e("response", response.toString());
+            @Override
+            public void response(String response) {
+                dialog.dismiss();
+                Log.e("response", response.toString());
 
-            try {
-                JSONObject obj = new JSONObject(response);
-                if (obj.getString("Response").equalsIgnoreCase("0")) {
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    if (obj.getString("Response").equalsIgnoreCase("0")) {
 
-                    Toast.makeText(LoginScreen.this,obj.getString("ResponseMsg").toString(),Toast.LENGTH_LONG).show();
+                        Toast.makeText(LoginScreen.this, obj.getString("ResponseMsg").toString(), Toast.LENGTH_LONG).show();
 
-                    User currentUser = new GsonBuilder().create().fromJson(response, User.class);
-                    //store current user and domain in shared preferences
-                    ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(LoginScreen.this, "user_pref", 0);
-                    complexPreferences.putObject("current_user", currentUser);
-                    complexPreferences.commit();
-                }else{
-                    Toast.makeText(LoginScreen.this,"Error - "+obj.getString("ResponseMsg").toString(),Toast.LENGTH_LONG).show();
+                        User currentUser = new GsonBuilder().create().fromJson(response, User.class);
+                        //store current user and domain in shared preferences
+                        ComplexPreferences complexPreferences = ComplexPreferences.getComplexPreferences(LoginScreen.this, "user_pref", 0);
+                        complexPreferences.putObject("current_user", currentUser);
+                        complexPreferences.commit();
+                    } else {
+                        Toast.makeText(LoginScreen.this, "Error - " + obj.getString("ResponseMsg").toString(), Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (Exception e) {
+
                 }
 
-            }catch (Exception e){
 
             }
 
-
-
-
-        }
-
-        @Override
-        public void error(VolleyError error) {
-            Log.e("volly er", error.toString());
-            dialog.dismiss();
-        }
-    }.start();
-}
+            @Override
+            public void error(VolleyError error) {
+                Log.e("volly er", error.toString());
+                dialog.dismiss();
+            }
+        }.start();
+    }
 
     public boolean isEmailMatch(EditText param1) {
         Pattern pattern = Patterns.EMAIL_ADDRESS;
         return pattern.matcher(param1.getText().toString()).matches();
     }
 
-    public boolean isEdiTextEmpty(EditText et){
+    public boolean isEdiTextEmpty(EditText et) {
 
         boolean isEmpty = false;
 
-        if(et.getText() == null || et.getText().toString().equalsIgnoreCase("")){
+        if (et.getText() == null || et.getText().toString().equalsIgnoreCase("")) {
             isEmpty = true;
         }
 
